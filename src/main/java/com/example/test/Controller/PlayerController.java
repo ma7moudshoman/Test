edbccb6aa1daa2155b745278fff16e719f4d4f75
ma7moudshoman <shomanm53@gmail.com>
2023-08-20 00:01:46 +0300

    Get Player

--- src/main/java/com/example/test/Controller/PlayerController.java
+++ src/main/java/com/example/test/Controller/PlayerController.java
@@ -5,9 +5,7 @@ import com.example.test.model.Player;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 //http://localhost:8088/
 @Controller
@@ -34,6 +32,12 @@ public class PlayerController {
  public String savePlayer(@ModelAttribute("player") Player player){
 playerRepo.save(player);
 return "redirect:/players";
+ }
+ @GetMapping("/getplayer")
+ public  String getplayer(@RequestParam("playerId") long id , Model model ){
+        model.addAttribute("player" , playerRepo.findById(id));
+        return "playerform";
+
  }
 
 }
